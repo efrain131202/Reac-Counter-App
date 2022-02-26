2db518c61f9c7f758d708f8054ae433da3116c89
Efrain <efraincruzlobato13@gmail.com>
2022-02-26 00:04:12 -0600

    Evento click

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -2,10 +2,18 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const CounterApp = ({ value }) => {
+
+    //hanledAdd
+
+    const hanledAdd = (e) =>{
+        console.log(e)
+    }
     return(
         <>
         <h1>CounterApp</h1>
         <h2> { value} </h2>
+
+        <button onClick={ (e) => { hanledAdd(e)}}>+1</button>
         </>
     )
 }
