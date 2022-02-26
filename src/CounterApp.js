298fde6836825fd8b10304c98741a07939970097
Efrain <efraincruzlobato13@gmail.com>
2022-02-26 00:19:02 -0600

    useState-hook

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 const CounterApp = ({ value }) => {
 
+    const [ counter, setCounter ] = useState(0);
+
+
+
     //hanledAdd
 
     const hanledAdd = (e) =>{
-        console.log(e)
+        //setCounter( counter + 1);
+        setCounter( (c) => c + 1 );
+        
     }
     return(
         <>
