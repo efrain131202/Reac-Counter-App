4633c86203cfb202eba9191ac8936a6e6f59c15a
Efrain <efraincruzlobato13@gmail.com>
2022-02-25 20:08:59 -0600

    Retonar elementos en el componente-Fragment

---
+++ src/PrimeraApp.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+// import React, { Fragment} from 'react'
+const PrimeraApp = () => {
+
+    return (
+        <>
+            <h1>Hola Mundo</h1>
+            <p> Mi Primera Aplicacion</p>
+        </>
+     );
+
+
+
+}
+
+export default PrimeraApp;
